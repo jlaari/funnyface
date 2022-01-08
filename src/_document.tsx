41d7c9551e533cb66a14143205ae0c4a35d50458
Jari Laari <jari.laari@vincit.com>
2022-01-08 13:58:57 -0800

    Add favicon

--- src/_document.tsx
+++ src/_document.tsx
@@ -19,6 +19,24 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
             as="style"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
